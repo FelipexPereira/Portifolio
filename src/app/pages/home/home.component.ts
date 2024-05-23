5408f92ea06a72b3fd53c8e21d60a57222fe6064
FelipexPereira <felipegabriel722@gmail.com>
2024-05-22 21:41:47 -0300

    att

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { MenuComponent } from '../../shared/components/menu/menu.component';
 import { CarrosselComponent } from '../../shared/components/carrossel/carrossel.component';
 import { DesenvolvimentoComponent } from '../../shared/components/desenvolvimento/desenvolvimento.component';
+import { MatCardModule } from '@angular/material/card';
+import { WelcomeComponent } from '../../shared/components/welcome/welcome.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuComponent, CarrosselComponent, DesenvolvimentoComponent],
+  imports: [
+    MenuComponent,
+    CarrosselComponent,
+    DesenvolvimentoComponent,
+    WelcomeComponent,
+    MatCardModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
---
+++ src/app/shared/components/welcome/welcome.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.scss'
+})
+export class WelcomeComponent {
+
+}
